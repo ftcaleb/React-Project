fade33d9199af4b4a9e640b84dcd3ab760091661
ftcaleb <caleb19scott@gmail.com>
2025-10-25 03:51:19 +0200

    Added code to header

--- React-portfolio-project/src/App.jsx
+++ React-portfolio-project/src/App.jsx
@@ -1,6 +1,22 @@
-import Header from './components/Header.';
+import React from 'react';
+import Header from './components/Header';
 import Skills from './components/Skills';
-import Home from './components/Home.';
-import About from './components/About.';
-import Portfolio from './components/Portfolio.';
-import Contact from './components/Contact.';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+    </div>
+  )
+
+}
+
---
+++ React-portfolio-project/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+export default function Header() {
+    return (
+        <div>
+            LOGO
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Hero</li>
+                    <li>Skills</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+        
+    )
+}
