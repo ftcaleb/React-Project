5aa382dd61b64813a2424d62422e65b0b3a5c248
ftcaleb <caleb19scott@gmail.com>
2025-10-27 21:50:16 +0200

    Import update header

--- React-portfolio-project/src/App.jsx
+++ React-portfolio-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import About from "./components/About";
