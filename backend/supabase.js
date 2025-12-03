2c36bf1b79fa2305f32275390a8c6791293df773
ftcaleb <caleb19scott@gmail.com>
2025-12-03 16:57:45 +0200

    FINAL FINAL UPDATE

--- backend/supabase.js
+++ backend/supabase.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!SUPABASE_URL || !SUPABASE_KEY) {
   console.error("Supabase URL or Key missing in .env");
